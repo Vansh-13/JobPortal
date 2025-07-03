bf27a23e08c3b676a6c4f04202318d50319bab80
Vansh Madaan <madaanvansh68@gmail.com>
2025-07-03 10:36:20 +0530

    database is created2

---
+++ backend/models/useraddress.models.js
@@ -0,0 +1,40 @@
+import mongoose  from "mongoose";
+
+
+const userAddressSchema=mongoose.Schema({
+    userID:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true
+    },
+    addressLine1:{
+        type: String,
+        required: true
+    },
+    addressLine2:{
+        type: String,
+        required: false
+    },
+    city:{
+        type: String,
+        required: true
+    },
+    state:{
+        type: String,
+        required: true
+    },
+    postalCode:{
+        type: String,
+        required: true
+    },
+    country:{
+        type: String,
+        required: true
+    }
+
+},{
+    timestamps: true
+})
+
+const UserAddress=mongoose.model("UserAddress",userAddressSchema);
+export default UserAddress;
